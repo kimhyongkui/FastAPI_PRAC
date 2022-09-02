7de6bc4a0c2d1776c3f7ea18208d37f1f33ed0b7
Kim Hyeong Kyu <hyongkui@nate.com>
2022-09-02 23:30:00 +0900

    복습

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    a = "a"
-    b = "b" + a
-    return {"hello world": b}
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
+
+
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer

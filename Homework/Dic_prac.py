0f7cfdc857c05be757028351a6fc70e0ff6bc876
Kim Hyeong Kyu <hyongkui@nate.com>
2022-09-28 23:26:04 +0900

    homework

--- Homework/Dic_prac.py
+++ Homework/Dic_prac.py
@@ -60,13 +60,13 @@ app = FastAPI
 #         print("잘못 선택하셨습니다.")
 #         break
 # print("내가 만든 영화 순위표 :", dict_movie)
-
-
+#
+#
 # 1. 왜 while이 2번 들어갔는가?
 # 2. 굳이 while을 2번 써야하는가?
 # 3. Append를 2번 사용했는데 굳이 이렇게 했어야 하는가?
 # 4. 딕셔너리를 n번 추가하기 -> n번 입력후 계속 추가할건지 물어보기
-
+#
 # 2. 수정후
 # dict_movie = []
 # while True:
@@ -90,7 +90,7 @@ app = FastAPI
 #         print("잘못 선택하셨습니다.")
 #         break
 # print("내가 만든 영화 순위표 :", *dict_movie, sep='\n')  #리스트 이름 앞에 "*"붙이면 괄호 생략함
-
+#
 # 3. 수정 2번째
 # tu_movie = []
 # dic_movie = {}
@@ -142,54 +142,3 @@ app = FastAPI
 #         else:
 #             currentRank = totalRank
 #         print(movie_list[i][0], '\t', movie_list[i][1], '\t', currentRank)
-
-# 4 함수로 만들기
-
-dict_movie = []
-
-
-# 반복할 횟수를 고르는 함수
-def count1():
-    count = int(input('반복할 횟수를 입력하세요: '))
-    if count > 0:
-        add(count)
-    else:
-        ask = input('종료를 원하면 yes, 아니면 no를 입력하세요: ')
-        if ask == 'yes':
-            print("종료합니다.")
-        else:
-            count1()
-
-
-# # 추가하는 함수
-# def add(count):
-#     for i in range(1, count + 1, 1):
-#         print("=====추가중======")
-#         name = input('제목: ')
-#         aud = input('관객수: ')
-#         rank = input('랭킹: ')
-#         dict_movie.append({'제목': name, '관객수': aud, '랭킹': rank})
-#         if i == count:
-#             finish = int(input('1번은 딕셔너리 요소 추가 계속, 2번은 종료 : '))
-#             if finish != 1:
-#                 print("===== 딕셔너리 요소 추가 끝 =====")
-
-# 추가하는 함수
-def add(count):
-    for i in range(1, count + 1, 1):
-        print("=====추가중======")
-        name = input('제목: ')
-        aud = input('관객수: ')
-        rank = input('랭킹: ')
-        dict_movie.append({'제목': name, '관객수': aud, '랭킹': rank})
-        if i == count:
-            finish = int(input('1번은 딕셔너리 요소 추가 계속, 2번은 종료 : '))
-            if finish != 1:
-                print("===== 딕셔너리 요소 추가 끝 =====")
-
-
-
-# print("내가 만든 영화 순위표 :", *dict_movie, sep='\n')  #리스트 이름 앞에 "*"붙이면 괄호 생략함
-
-print(count1())
-print(dict_movie, sep='\n')

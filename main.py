8dddb7f47506f8bed67fe002d79f4050ff695c0d
Kim Hyeong Kyu <hyongkui@nate.com>
2022-10-04 23:00:19 +0900

    FastAPI CRUD

--- main.py
+++ main.py
@@ -6,3 +6,30 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/user/", tags=["User"])
+async def read_users():
+    result = "내 이름은 김형규고 나이는 32살입니다."
+    return result
+
+
+@app.post("/user/", tags=["User"])
+async def users(name: name, age: age):
+    result = f"내 이름은 {name.name}고 나이는 {age.age}살입니다."
+    return result
+
+
+@app.patch("/user/", tags=["User"])
+async def update_users():
+    result = "내 이름은 누구고 나이는 몇살입니다."
+    return result
+
+
+@app.delete("/user/", tags=["User"])
+async def items():
+    result = "내 이름은 누구고 나이는 몇살입니다."
+    return result
+
+@app.get("/items/", tags=["공부"], response_model=Item)
+async def read_item(item_id):
+    return items[item_id]

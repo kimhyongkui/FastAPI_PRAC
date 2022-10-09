752b7248f5b72caa5e6d501fe7a9f33980cdf9e6
Kim Hyeong Kyu <hyongkui@nate.com>
2022-10-09 23:31:58 +0900

    check

---
+++ Homework/2.py
@@ -0,0 +1,8 @@
+
+
+def Output(a, b):
+    add = a +b
+    print(a,'+',b,'=',add,'입니다.', sep='')
+    return add
+
+print(Output(3,8))
--- MySQL_Prac/MySQL_example.py
+++ MySQL_Prac/MySQL_example.py
@@ -25,6 +25,11 @@ cursor = conn.cursor()
 #     );
 # '''
 #테이블 생성(삽입)
+for index in range(10):
+    product_code = 215673140 + index + 1
+    sql = """INSERT INTO product VALUES(
+    '""" + str(product_code) + """', '스위트바니 여름신상5900원~롱원피스티셔츠/긴팔/반팔', 23000, 6900, 70, 'F'); """
+    print(sql)
 
 #데이터 검색(조회)
 

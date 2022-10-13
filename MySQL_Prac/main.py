064584c58f571032e1b9ea8b1078d23c0043f3c2
Kim Hyeong Kyu <hyongkui@nate.com>
2022-10-13 23:45:38 +0900

    re

--- MySQL_Prac/main.py
+++ MySQL_Prac/main.py
@@ -11,16 +11,26 @@ class City(BaseModel):
     name: str
     timezone: str
 
-# Create-post / Read-get / Update-put,patch / Delete-delete
+class CityModify(BaseModel):
+    id: int
+    name: str
+    timezone: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 
 @app.get('/cities')
 def get_cities():
     results = []
     for city in db:
-        strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
-        r = requests.get(strs)
+        str = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
+        print(str)
+        r = requests.get(str)
         cur_time = r.json()['datetime']
-        results.append({'name': city['name'], 'timezone': city['timezone'], 'current_time': cur_time})
+        results.append({'name':city['name'], 'timezone':city['timezone'], 'current_time': cur_time})
 
     return results
 
@@ -28,22 +38,28 @@ def get_cities():
 @app.get('/cities/{city_id}')
 def get_city(city_id: int):
     city = db[city_id-1]
-    strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
-    r = requests.get(strs)
+    r = requests.get(f"http://worldtimeapi.org/api/timezone/{city['timezone']}")
     cur_time = r.json()['datetime']
-
-    return {'name': city['name'], 'timezone': city['timezone'], 'current_time': cur_time}
+    return {'name':city['name'], 'timezone':city['timezone'], 'current_time': cur_time}
 
 
 @app.post('/cities')
 def create_city(city: City):
     db.append(city.dict())
-
     return db[-1]
 
+@app.put('/cities')
+def modify_city(city: CityModify):
+
+    db[city.id-1] = { 'name': city.name, 'timezone': city.timezone }
+
+    return db[city.id-1]
 
 @app.delete('/cities/{city_id}')
-def create_city(city_id: int):
+def delete_city(city_id: int):
     db.pop(city_id-1)
-
     return {}
+
+
+
+

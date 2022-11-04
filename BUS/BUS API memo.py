9fe77302f6e29c0f54e656b7e694dab10ee9a358
Kim Hyeong Kyu <hyongkui@nate.com>
2022-11-04 23:48:51 +0900

    bus

--- BUS/BUS API memo.py
+++ BUS/BUS API memo.py
@@ -5,7 +5,6 @@ app = FastAPI()
 
 
 key = "sh%2F1QzN2LTDtEC%2BJVBs0xY8tKrpfWk%2F5uHe88YcwMk59ICjn2dhJ6tSBL5DnWTkBDlyn5YRqJR1IQPXex6TqFQ%3D%3D"
-# url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?ServiceKey={}&busRouteId=100100118".format(key)
 url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey={}".format(key)
 # .format을 이용해서 포매팅
 
@@ -32,15 +31,16 @@ jsonObj = json.loads(jsonString) # JSON 디코딩, json을 dict으로 변환
 #             busRouteNm == jsonObj[i]['busRouteId']
 #     return jsonObj[i]['busRouteId']
 
-
+bus_list = {"bus_name":"bus_Id"}
 def getBusRouteId():
-    bus_list = []
+    list = []
     for bus in jsonObj:
         bus_name = bus['busRouteNm']
         bus_Id = bus['busRouteId']
-        bus_list.append((bus_name, bus_Id))
+        list.append((bus_name, bus_Id))
         result = f'{bus_name}의 버스ID는 {bus_Id}입니다.'
-
+        print(result)
     return result
 
-print(getBusRouteId())
+print(result)
+

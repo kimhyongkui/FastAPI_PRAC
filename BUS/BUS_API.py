e3a71654be21e2d0f892f2a72110308a912dd72e
Kim Hyeong Kyu <hyongkui@nate.com>
2022-11-14 22:31:20 +0900

    env

--- BUS/BUS_API.py
+++ BUS/BUS_API.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
 import requests, xmltodict, json
+import os
+from dotenv import load_dotenv
 
-app = FastAPI()
 
 
-key = "sh%2F1QzN2LTDtEC%2BJVBs0xY8tKrpfWk%2F5uHe88YcwMk59ICjn2dhJ6tSBL5DnWTkBDlyn5YRqJR1IQPXex6TqFQ%3D%3D"
-url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getStaionByRoute?ServiceKey={}&busRouteId=100100118".format(key)
+load_dotenv()
+key = os.getenv('key')
+
+url = f"http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?ServiceKey={key}"
 
 
 
